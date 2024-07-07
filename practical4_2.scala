4ec028d8e71de615aa5dc7f22dd7cf76c8926a90
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-07-07 05:00:52 -0700

    Update practical4_2.scala

--- practical4_2.scala
+++ practical4_2.scala
@@ -1,17 +1,13 @@
 object practical4_2 {
   def main(args: Array[String]): Unit = {
     if (args.length > 0) {
-      try {
-        val input = args(0).toInt
-        val result = input match {
-          case x if x <= 0     => "Nagative/Zero is input"
-          case x if x % 2 == 0 => "Even number is given"
-          case _               => "Odd number is given"
-        }
-        println(result)
-      } catch {
-        case e: NumberFormatException => println("Please enter a valid integer")
+      val input = args(0).toInt
+      val result = input match {
+        case x if x <= 0     => "Nagative/Zero is input"
+        case x if x % 2 == 0 => "Even number is given"
+        case _               => "Odd number is given"
       }
+      println(result)
     } else {
       println("Please provide an integer input")
     }

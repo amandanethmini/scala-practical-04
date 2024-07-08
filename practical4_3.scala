fc4db30b4aa5bb94b5475da4e43d488cd1ca79fa
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-07-07 21:56:21 -0700

    Update practical4_3.scala

--- practical4_3.scala
+++ practical4_3.scala
@@ -1,24 +1,36 @@
 object practical4_3 {
-  def toUpper(str: String): String = {
-    str.toUpperCase
+
+  def toUpper(name: String): String = {
+    name.toUpperCase()
   }
 
-  def toLower(str: String): String = {
-    str.toLowerCase
+  def toLower(name: String): String = {
+    name.toLowerCase()
   }
 
-  def formatNames(name: String)(formatter: String => String): String = {
-    formatter(name)
+  def formatNames(name: String)(formatFunction: String => String): String = {
+    formatFunction(name)
   }
 
   def main(args: Array[String]): Unit = {
-    val names = List("Benny", "Niroshan", "Saman", "Kumara")
-    names.foreach { name =>
-      val formattedName = formatNames(name) {
-        case s if s == s.toUpperCase => toLower(s)
-        case _                       => toUpper(name)
-      }
-      println(formattedName)
-    }
+    val n1 = "Benny"
+    val n2 = "Niroshan"
+    val n3 = "Saman"
+    val n4 = "Kumara"
+
+    println(formatNames(n1)(toUpper))
+    println(
+      formatNames(n2)(name =>
+        name.substring(0, 2).toUpperCase + name.substring(2)
+      )
+    ) // Output: NIroshan
+    println(formatNames(n3)(toLower))
+    println(
+      formatNames(n4)(name =>
+        name.substring(0, 1).toUpperCase + name
+          .substring(1, 5)
+          .toLowerCase + name.substring(5).toUpperCase
+      )
+    ) // Output: KumarA
   }
 }
